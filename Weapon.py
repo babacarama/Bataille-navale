21da4d05662ce14270d3136284eabe576fb37494
BILA Ahmed Kader <ahmed.bila@centrale-casablanca.ma>
2023-10-19 10:02:55 +0100

    new version

--- Weapon.py
+++ Weapon.py
@@ -1,8 +1,8 @@
 class Weapon:
+    
     def __init__(self, ammunitions: int, range: int):
         self.ammunitions = ammunitions
         self.range = range
 
     def fire_at(self, x: int, y: int, z: int):
         print(f"Firing at coordinates ({x}, {y}, {z})") 
-        

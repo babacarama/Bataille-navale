e51bedaa3e98e363aeec5a84881d8e205931bb33
tanoh <tanohfergusson1209@gmail.com>
2023-11-21 09:44:07 +0100

    test

---
+++ interface.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+# Créer une fenêtre
+window = tk.Tk()
+
+# Créer un titre
+label = tk.Label(text="fergo logo", font=("Times New Roman", 30), fg="red")
+label.pack()
+
+# Démarrer la boucle d'événements
+window.mainloop()

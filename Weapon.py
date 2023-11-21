aa6c6fd8da8b6f484c15029c9d1553319b800c5e
babacarama <babacaramar02@gmail.com>
2023-11-21 02:39:47 +0100

    teste

--- Weapon.py
+++ Weapon.py
@@ -1,7 +1,10 @@
 class Weapon:
     def __init__(self, ammunitions: int, range: int):
         self.ammunitions = ammunitions
-        self.range = range
+        self.range = range      
         
     def fire_at(self, x: int, y: int, z: int):
+        self.x=x
+        self.y=y
         print(f"Tir aux coordonnées ({x}, {y}, {z})")
+                
